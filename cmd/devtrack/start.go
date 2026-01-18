6dcc1572c1761d142268ade260fd6cf2e18597ed
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2026-01-18 11:18:17 +0000

    Refactor start command implementation for clarity and consistency

--- cmd/devtrack/start.go
+++ cmd/devtrack/start.go
@@ -2,20 +2,37 @@ package devtrack
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start tracking a new work",
-	Long:  "Start a new work session and begin trace work..",
+	Long:  "Start a new work session and begin tracking",
 	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		task := args[0]
-		fmt.Println("Started tracking:", task)
-	},
+	Run:   startTracking,
+}
+
+func startTracking(cmd *cobra.Command, args []string) {
+	task := args[0]
+
+	project, _ := cmd.Flags().GetString("project")
+
+	fmt.Println("Started tracking:", task)
+
+	if project != "" {
+		fmt.Println("Project:", project)
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+
+	startCmd.Flags().StringP(
+		"project", // flag name
+		"p",       // shorthand
+		"",        // default value
+		"Project name", //descrtiption
+	)
 }
